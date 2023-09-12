6dc82ff3641b705c1fc28fb79aeb3fa965f4c946
Eduardito187 <eduardchavez302@gmail.com>
2023-09-12 04:13:14 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\MiddlewareCustom;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware([MiddlewareCustom::class])->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
